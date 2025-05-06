5c73500f650910bb7d15fc4a55fe28c8fa6c0cb2
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2025-05-06 23:00:15 +0200

    Skill Tree Paths
    Added/improved paths between skills in the skill tree. Also changed fandoms.json so it can actually parse without error.

--- scripts/path_2d.gd
+++ scripts/path_2d.gd
@@ -1,16 +1,15 @@
 extends Node2D
 
-var line = Line2D.new()
-
 func draw_paths():
 	var nodes = get_tree().get_nodes_in_group("SkillButtons")
 	for node in nodes:
-		print(node.connect_skills)
 		for dependant in node.connect_skills:
+			var line = Line2D.new()
 			var start_point = node.position + node.size / 2
 			var end_point = dependant.position + dependant.size / 2
 			line.add_point(start_point, 0)
 			line.add_point(end_point, 1)
-			line.joint_mode = Line2D.LINE_JOINT_ROUND
 			line.width = 2
+			line.default_color = Color.DARK_ORANGE
 			add_child(line)
+			print("Drew a line between: ", node, " and ", dependant)
--- scripts/skill_tree.gd
+++ scripts/skill_tree.gd
@@ -2,5 +2,22 @@ extends Control
 
 @onready var path_drawer: Node2D = $PathDrawer
 
+var missing_buttons: Array = []
+
 func _ready() -> void:
 	path_drawer.draw_paths()
+	skill_button_check()
+
+func skill_button_check():
+	var skill_data = SkillsData.skills_dict.keys()
+	var node_paths = get_tree().get_nodes_in_group("SkillButtons")
+	var button_names = []
+	
+	for node in node_paths:
+		button_names.append(node.name)
+	
+	for skill in skill_data:
+		if skill not in button_names:
+			missing_buttons.append(skill)
+	
+	print("The following skills are missing a button: ", missing_buttons)
