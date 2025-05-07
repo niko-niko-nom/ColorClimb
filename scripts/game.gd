d944993ff132f83bdb52942be7b1b44841a22620
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2025-05-07 13:22:10 +0200

    Order
    Changed the order in which functions and data is loaded, to ensure other functions dependent on them can run.

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,8 +1,12 @@
 extends Control
 
 func _ready():
-	PlayerStats.check_for_unlocks()
 	ActivitiesData.load_activities()
+	
 	if PlayerStats.first_startup:
 		GlobalConfigFile.start_new_game()
+		#PlayerStats.set_startup_stats()
+		PlayerStats.startup_fandoms()
 		PlayerStats.first_startup = false
+	
+	PlayerStats.check_for_unlocks()
